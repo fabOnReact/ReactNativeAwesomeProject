dd78fef78adbd2d02526f3b7d652db48027a4bf4
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-01-14 08:41:02 +0800

    test with ImageBackground

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import type {Node} from 'react';
 
 import React from 'react';
 import {
+  Button,
+  ImageBackground,
   StyleSheet,
   Text,
   useColorScheme,
@@ -33,36 +35,35 @@ const App: () => Node = () => {
   };
 
   return (
-    <View>
-      <View
-        onClick={() => console.log('click')}
-        accessible={true}
-        accessibilityRole="button"
-        accessibilityActions={accessibilityAction}
-        // accessibilityLabel="this is custom label"
-        style={{backgroundColor: 'red', height: 100, marginTop: 150}}>
-        <Text>Some Other Text</Text>
-      </View>
+    <View style={styles.container}>
+      <Button
+        onPress={() => console.log('onPress')}
+        importantForAccessibility={'no'}
+        title="This button is still focusable, bt shouldn't be"
+      />
+      <ImageBackground
+        style={{width: 50, height: 50}}
+        source={{uri: 'https://reactnative.dev/img/opengraph.png'}}
+        importantForAccessibility={'no-hide-descendants'}>
+        <Text>This element should not be focusable, but it is</Text>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  paragraph: {
+    margin: 24,
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
