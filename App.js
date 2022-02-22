2c9773c5541ac8df8a2e1ef809e293a55855f4cb
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-02-22 09:01:16 +0800

    adding accessibilityAction examples

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import {Text, Button, View} from 'react-native';
+import {Text, Button, View, TouchableOpacity, Pressable} from 'react-native';
 
 class App extends React.Component {
   constructor(props) {
@@ -42,18 +42,20 @@ class App extends React.Component {
           title="Testing"
           onPress={() => console.warn('pressed')}
         />
-        <View
+        <TouchableOpacity
           accessible={true}
+          onPress={() => console.log('pressed')}
           accessibilityActions={[{name: 'activate'}]}
           onAccessibilityAction={event => {
             switch (event.nativeEvent.actionName) {
               case 'activate':
                 Alert.alert('Alert', 'View is clicked');
+                console.log('activate');
                 break;
             }
           }}>
           <Text>Click me</Text>
-        </View>
+        </TouchableOpacity>
         <View
           accessible={true}
           accessibilityActions={[
@@ -76,6 +78,44 @@ class App extends React.Component {
           }}>
           <Text>This view supports many actions.</Text>
         </View>
+        <TouchableOpacity
+          accessible={true}
+          accessibilityActions={[
+            {name: 'cut', label: 'cut'},
+            {name: 'copy', label: 'copy'},
+            {name: 'paste', label: 'paste'},
+            {name: 'longpress', label: 'longpress'},
+          ]}
+          onLongPress={() => console.log('onLongPress')}
+          onAccessibilityAction={event => {
+            console.log('event', event);
+            switch (event.nativeEvent.actionName) {
+              case 'cut':
+                Alert.alert('Alert', 'cut action success');
+                break;
+              case 'copy':
+                Alert.alert('Alert', 'copy action success');
+                break;
+              case 'paste':
+                Alert.alert('Alert', 'paste action success');
+                break;
+            }
+          }}>
+          <Text selectable={true}>Copy me</Text>
+        </TouchableOpacity>
+        <Pressable
+          style={{width: 100, height: 100, backgroundColor: 'pink'}}
+          accessibilityActions={[
+            {
+              name: 'activate',
+              label: 'Download file',
+            },
+          ]}
+          onPress={() =>
+            console.warn('Never called in accessibility role. =( !')
+          }>
+          <Text>message text</Text>
+        </Pressable>
       </>
     );
   }
